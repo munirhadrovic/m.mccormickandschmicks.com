178e34464ab54f00f60dc93b7746a06ad0dd301f
anes89cok <anes89cok@gmail.com>
2015-05-04 15:41:08 +0200

    First commit

---
+++ settings.php
@@ -0,0 +1,37 @@
+
+<?php
+$adresa[0]="321 West Katella Avenue Anaheim, CA 92802"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+$grad[0]="";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+$skracenica[0]=""; // AKo ne znate skracenicu ostavite praznu
+$nazivlokacije[0]="California";
+$telephone[0]=7145359000;// Broj koji se poziva na dugme CALL US
+
+$adresa[1]="1659 Wazee St Denver, CO 80202"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+$grad[1]="Denver";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+$skracenica[1]=""; // AKo ne znate skracenicu ostavite praznu
+$nazivlokacije[1]="Colorado";
+$telephone[1]=3038251107;// Broj koji se poziva na dugme CALL US
+
+$adresa[2]="9114 Strada Place Naples, FL 34108"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+$grad[2]="Naples";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+$skracenica[2]="FL"; // AKo ne znate skracenicu ostavite praznu
+$nazivlokacije[2]="Florida";
+$telephone[2]=2395912299;// Broj koji se poziva na dugme CALL US
+
+$adresa[3]="600 Ashwood Parkway Atlanta, GA 30338"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+$grad[3]="";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+$skracenica[3]="GA"; // AKo ne znate skracenicu ostavite praznu
+$nazivlokacije[3]="Georgia";
+$telephone[3]=7703999900;// Broj koji se poziva na dugme CALL US
+
+$adresa[4]="41 E. Chestnut St. Chicago, IL 60611"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+$grad[4]="Chicago";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+$skracenica[4]="IL"; // AKo ne znate skracenicu ostavite praznu
+$nazivlokacije[4]="Illinois";
+$telephone[4]=3123979500;// Broj koji se poziva na dugme CALL US
+
+$companynumber=5;
+$theme="b";//Thema koja se aplicira na sve
+$transitionefect="turn"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup", "turn", "slidefade", "flow"
+$transitionefect1="fade"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup", "turn", "slidefade", "flow"
+?>
